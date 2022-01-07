fb90c0836074832f999385771de8f953ab639f18
JoviWW <josevww@gmail.com>
2022-01-07 18:12:44 -0300

    Update get_receitas_feed.php

--- get_receitas_feed.php
+++ get_receitas_feed.php
@@ -9,25 +9,28 @@ $result = pg_query($con, "SELECT * FROM Receita ORDER BY Data desc;");
 if ( pg_num_rows ($result) > 0) {
 
     $response["receitas"] = array();
-
+/*
     while ($row = pg_fetch_query($result)){
         $receita = array();
         $receita["id"] = $row["id"];
         $receita["Nomerec"] = $row["Nomerec"];
-        
+        $receita["Sobre"] = $row["Sobre"];
+        $receita["Ingredientes"] = $row["Ingredientes"];
+        $receita["Preparo"] = $row["Preparo"];
+        //$receita["Imagem"] = $row["Imagem"];
         array_push($response["receitas"], $receita);
 
     }
-    
+*/  array_push($response["receitas"], "Oi");
     echo json_encode($response);
 
-} else{
+} /*else{
   
     $response["success"] = 0;
     $response["msg"] = "Nao ha produtos";
     echo json_encode($response);
     
-}
+}*/
 pg_close($con);
 
 ?>
