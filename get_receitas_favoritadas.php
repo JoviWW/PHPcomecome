8a8064cc2e61daed0574cfa743c4a982e5c6c3a2
JoviWW <josevww@gmail.com>
2022-01-14 00:23:03 -0300

    php fragSeguidos + perfil

---
+++ get_receitas_favoritadas.php
@@ -0,0 +1,38 @@
+<?php
+
+$response = array();
+$con = pg_connect(getenv("DATABASE_URL"));
+
+if(isset($_GET["codusu"])){
+$codusu = $_GET["codusu"];
+$result = pg_query($con, "SELECT * from usuario usu join favorito fav on (fav.codusufk = usu.codusu) join receita rec on (fav.codreceitafk = rec.codreceita) where usu.codusu = $codusu; ");
+if ( pg_num_rows ($result) > 0) {
+    $response["success"] = 1;
+    $response["receitas"] = array();
+
+    while ($row = pg_fetch_array($result)){
+        $receita = array();
+        $receita["codreceita"] = $row["codreceita"];
+        $receita["nomerec"] = $row["nomerec"];
+        $receita["sobre"] = $row["sobre"];
+        //$receita["Imagem"] = $row["Imagem"];
+        array_push($response["receitas"], $receita);
+
+    }
+    
+    echo json_encode($response);
+
+} else{
+  
+    $response["success"] = 0;
+    $response["msg"] = "Nao ha receitas favoritadas";
+    echo json_encode($response);
+    
+}
+} else { 
+    $response["success"] = 0;
+    $response["msg"] = "Codigo de usuario nao recebido.";
+    echo json_encode($response);
+ }
+pg_close($con);
+?>
---
+++ get_usuarios_seguidos.php
@@ -0,0 +1,34 @@
+<?php
+
+$response = array();
+$con = pg_connect(getenv("DATABASE_URL"));
+
+if(isset($_GET["codusu"])){
+$codusu = $_GET["codusu"];
+$result = pg_query($con, "SELECT ususeg.* from usuario usu join seguidos seg on (seg.seguindo = usu.codusu) join usuario ususeg on (seg.seguido = ususeg.codusu) where usu.codusu = $codusu;");
+if ( pg_num_rows ($result) > 0) {
+    $response["success"] = 1;
+    $response["receitas"] = array();
+
+    while ($row = pg_fetch_array($result)){
+        $usuario = array();
+        $usuario["codusu"] = $row["codusu"];
+        $usuario["sobreusu"] = $row["sobreusu"];
+        //$usuario["imagem"] = $row["imagem"];
+        array_push($response["usuarios"], $receita);
+
+    }
+    
+    echo json_encode($response);
+
+} else{
+  
+    $response["success"] = 0;
+    $response["msg"] = "Nenhum usuário é seguido.";
+    echo json_encode($response);
+    
+}
+}
+
+pg_close($con);
+?>
