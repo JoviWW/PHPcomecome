4ec4f7c86fb06014915cf2d46887a3f8a7a4af30
JoviWW <josevww@gmail.com>
2022-01-24 02:29:45 -0300

    Update get_usuarios_seguidos.php

--- get_usuarios_seguidos.php
+++ get_usuarios_seguidos.php
@@ -4,11 +4,13 @@ $response = array();
 $con = pg_connect(getenv("DATABASE_URL"));
 
 if(isset($_GET["codusu"])){
+
 $codusu = $_GET["codusu"];
+
 $result = pg_query($con, "SELECT ususeg.* from usuario usu join seguidos seg on (seg.seguindo = usu.codusu) join usuario ususeg on (seg.seguido = ususeg.codusu) where usu.codusu = $codusu;");
 if ( pg_num_rows ($result) > 0) {
     $response["success"] = 1;
-    $response["receitas"] = array();
+    $response["usuarios"] = array();
 
     while ($row = pg_fetch_array($result)){
         $usuario = array();
