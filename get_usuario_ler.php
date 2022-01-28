94f127d9eeebecb8fe1a01336926dc79db5fbc9a
JoviWW <josevww@gmail.com>
2022-01-28 13:11:19 -0300

    Update get_usuario_ler.php

--- get_usuario_ler.php
+++ get_usuario_ler.php
@@ -2,16 +2,18 @@
 $response = array();
 $con = pg_connect(getenv("DATABASE_URL"));
 
-if(isset($_GET["codusu"])){
+if(isset($_GET["codusu"]) && isset($_GET["codseguindo"])){
 $codusu = $_GET["codusu"];
+$codseguindo = $_GET["codseguindo"];
 $result = pg_query($con, "SELECT * FROM usuario WHERE(codusu = $codusu);");
     if(!empty($result)){
         if ( pg_num_rows ($result) > 0) {
-        
+            $resultver = pg_query($con, "SELECT Count(*) as seg FROM seguidos WHERE(codseguido = '$codusu' and codseguindo = '$codseguindo')");
           $resultnumseg = pg_query($con, "SELECT Count(*) as numseg FROM seguidos where(seguido=$codusu);");
           $resultnumrec = pg_query($con, "SELECT Count(*) as numrec FROM receita where(autor=$codusu);");
           $result = pg_fetch_array($result);
           $resultnumseg  = pg_fetch_array($resultnumseg );
+          $resultver  = pg_fetch_array($resultver );
           $resultnumrec = pg_fetch_array($resultnumrec);
           $usuario = array();
           $usuario["nome"] =  $result["nome"];
@@ -19,6 +21,7 @@ $result = pg_query($con, "SELECT * FROM usuario WHERE(codusu = $codusu);");
           $usuario["numseg"] =  $resultnumseg["numseg"];
           $usuario["numrec"] =  $resultnumrec["numrec"];
           $usuario["imagem"] = $result["imagem"];
+          $usuario["seg"] =  $resultver["seg"];
         $response["success"] = 1;
         $response["usuario"] = array();
         array_push($response["usuario"], $usuario);
