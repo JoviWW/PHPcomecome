63b7561c56fdb1bedbb8102f5996418fdce364cb
JoviWW <josevww@gmail.com>
2022-01-28 02:31:07 -0300

    update

--- get_receita_ler.php
+++ get_receita_ler.php
@@ -3,12 +3,15 @@
 $response = array();
 $con = pg_connect(getenv("DATABASE_URL"));
 
-if(isset($_GET["codreceita"])){
+if(isset($_GET["codreceita"]) && isset($_GET["codusu"])){
 $codreceita = $_GET["codreceita"];
+$codusu = $_GET["codusu"];
 $result = pg_query($con, "SELECT * FROM receita WHERE(codreceita = $codreceita);");
     if(!empty($result)){
         if ( pg_num_rows ($result) > 0) {
+            $resultver = pg_query($con, "SELECT * FROM favorito WHERE(codusu = '$codusu' and codreceita = '$codreceita')");
 
+            if ( pg_num_rows ($resultver) > 0) {$receita["favorito"] = true;} else { $receita["favorito"] = false;}
           $result = pg_fetch_array($result);
           $receita = array();
           $receita["nomerec"] =  $result["nomerec"];
