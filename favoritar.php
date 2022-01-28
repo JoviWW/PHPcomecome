9affaa1921ce1c3aa176b9f05ed6362c81f33fbf
JoviWW <josevww@gmail.com>
2022-01-28 02:21:53 -0300

    Update favoritar.php

--- favoritar.php
+++ favoritar.php
@@ -7,23 +7,25 @@ if(isset($_POST["codusu"]) && isset($_POST["codreceita"])){
 $codusu = $_POST['codusu'];
 $codreceita = $_POST['codreceita'];
 $con = pg_connect(getenv("DATABASE_URL"));
+$resultver = pg_query($con, "SELECT * FROM Receita ORDER BY Data desc;");
+
+if ( pg_num_rows ($resultver) > 0) {
+    $response["success"] = 2;
+    $response["msg"] = "Favorito já existente.";
+    $resulterminar = pg_query($con, "DELETE FROM favorito WHERE(codusu = '$codusu' and codreceita = '$codreceita');");
+    pg_close($con);
+    echo json_encode($response);
+} else {
+
 $result = pg_query($con, "INSERT INTO favorito(codreceita,codusu) VALUES ('$codreceita','$codusu');");
 if($result){
     $response["success"] = 1;
     $response["msg"] = "Produto criado com sucesso";
 
     pg_close($con);
-    echo json_encode($response);
-} else {
-    $response["success"] = 2;
-    $response["msg"] = "Favorito já existente.";
-    $resulterminar = pg_query($con, "DELETE FROM favorito WHERE(codusu = '$codusu' and codreceita = '$codreceita');");
-    pg_close($con);
-    echo json_encode($response);
+    echo json_encode($response);}
 }
-
-
-} else {
+}else {
      $response["success"] = 0;
     $response["msg"] = "Parametros não atendidos";
 
